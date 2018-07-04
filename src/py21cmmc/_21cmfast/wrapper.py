bc9732f61346b55cc5e3e973c391157b00a253ed
Steven Murray <steven.murray@curtin.edu.au>
2018-07-04 14:43:00 +0800

    Auto stash before merge of "master" and "origin/master"

--- src/py21cmmc/_21cmfast/wrapper.py
+++ src/py21cmmc/_21cmfast/wrapper.py
@@ -103,11 +103,11 @@ class InitialConditions:
         # self.lowres_vx_2LPT = np.zeros(box_dim.HII_tot_num_pixels)
         # self.lowres_vy_2LPT = np.zeros(box_dim.HII_tot_num_pixels)
         # self.lowres_vz_2LPT = np.zeros(box_dim.HII_tot_num_pixels)
-        self.hires_density = np.zeros(box_dim.tot_fft_num_pixels,dtype=np.float32)
+        self.hires_density = np.zeros(box_dim.tot_fft_num_pixels, dtype=np.float32)
 
         # Put everything in the struct
         self.cstruct = ffi.new("struct InitialConditions*")
-        self.cstruct.hires_density = ffi.cast("float", ffi.from_buffer(self.hires_density))
+        self.cstruct.hires_density = ffi.cast("float *", ffi.from_buffer(self.hires_density))
 
 
 class PerturbedField:
@@ -147,73 +147,74 @@ def initial_conditions(user_params, cosmo_params, regenerate=False, write=True):
 
     return boxes
 
-def perturb_field(redshift, init_boxes,  write=True, regenerate=False, read=False):
-    """
-    Compute a perturbed field at a given redshift.
 
-    Parameters
-    ----------
-    redshift
-    init_boxes
-    write
-    regenerate
-    read
-    dir
-
-    Returns
-    -------
-
-    """
-    # First initialize perturbed boxes.
-    fields = PerturbedField(len(init_boxes.lowres_vx))
-
-    # Run the C Code
-    lib.ComputePerturbField(redshift, init_boxes, fields)
-
-    # Optionally do stuff with the result (like writing it)
-    if write:
-        pass
-
-    return fields
-
-
-def ionize(redshifts, flag_options, astro_params):
-    for z in redshifts:
-        lib.ComputeIonisationBoxes(z, z+0.2, flag_options, astro_params)
-
-    return something
-
-
-def run_21cmfast(redshifts, box_dim=None, flag_options=None, astro_params=None, cosmo_params=None,
-                 write=True, regenerate=False, run_perturb=True, run_ionize=True, init_boxes=None,
-                 free_ps=True, progress_bar=True):
-
-    # Create structures of parameters
-    box_dim = box_dim or {}
-    flag_options = flag_options or {}
-    astro_params = astro_params or {}
-    cosmo_params = cosmo_params or {}
-
-    box_dim = BoxDim(**box_dim)
-    flag_options = FlagOptions(**flag_options)
-    astro_params = AstroParams(**astro_params)
-    cosmo_params = CosmoParams(**cosmo_params)
-
-    # Compute initial conditions, but only if they aren't passed in directly by the user.
-    if init_boxes is None:
-        init_boxes = initial_conditions(box_dim, cosmo_params, regenerate, write)
-
-    output = [init_boxes]
-
-    # Run perturb if desired
-    if run_perturb:
-        for z in redshifts:
-            perturb_fields = perturb_field(z, init_boxes, regenerate=regenerate)
-
-    # Run ionize if desired
-    if run_ionize:
-        ionized_boxes = ionize(redshifts, flag_options, astro_params)
-        output += [ionized_boxes]
-
-    return output
+# def perturb_field(redshift, init_boxes,  write=True, regenerate=False, read=False):
+#     """
+#     Compute a perturbed field at a given redshift.
+#
+#     Parameters
+#     ----------
+#     redshift
+#     init_boxes
+#     write
+#     regenerate
+#     read
+#     dir
+#
+#     Returns
+#     -------
+#
+#     """
+#     # First initialize perturbed boxes.
+#     fields = PerturbedField(len(init_boxes.lowres_vx))
+#
+#     # Run the C Code
+#     lib.ComputePerturbField(redshift, init_boxes, fields)
+#
+#     # Optionally do stuff with the result (like writing it)
+#     if write:
+#         pass
+#
+#     return fields
+#
+#
+# def ionize(redshifts, flag_options, astro_params):
+#     for z in redshifts:
+#         lib.ComputeIonisationBoxes(z, z+0.2, flag_options, astro_params)
+#
+#     return something
+#
+#
+# def run_21cmfast(redshifts, box_dim=None, flag_options=None, astro_params=None, cosmo_params=None,
+#                  write=True, regenerate=False, run_perturb=True, run_ionize=True, init_boxes=None,
+#                  free_ps=True, progress_bar=True):
+#
+#     # Create structures of parameters
+#     box_dim = box_dim or {}
+#     flag_options = flag_options or {}
+#     astro_params = astro_params or {}
+#     cosmo_params = cosmo_params or {}
+#
+#     box_dim = BoxDim(**box_dim)
+#     flag_options = FlagOptions(**flag_options)
+#     astro_params = AstroParams(**astro_params)
+#     cosmo_params = CosmoParams(**cosmo_params)
+#
+#     # Compute initial conditions, but only if they aren't passed in directly by the user.
+#     if init_boxes is None:
+#         init_boxes = initial_conditions(box_dim, cosmo_params, regenerate, write)
+#
+#     output = [init_boxes]
+#
+#     # Run perturb if desired
+#     if run_perturb:
+#         for z in redshifts:
+#             perturb_fields = perturb_field(z, init_boxes, regenerate=regenerate)
+#
+#     # Run ionize if desired
+#     if run_ionize:
+#         ionized_boxes = ionize(redshifts, flag_options, astro_params)
+#         output += [ionized_boxes]
+#
+#     return output
 
