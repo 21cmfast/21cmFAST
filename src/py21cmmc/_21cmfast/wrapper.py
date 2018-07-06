f1698e4bf678537ce2a4a7c621256f7ce6a78667
Steven Murray <steven.murray@curtin.edu.au>
2018-07-06 11:59:03 +0800

    add perturb field calculations

--- src/py21cmmc/_21cmfast/wrapper.py
+++ src/py21cmmc/_21cmfast/wrapper.py
@@ -121,13 +121,23 @@ class InitialConditions(OutputStruct):
         self.hires_density = np.zeros(self.user_params.tot_fft_num_pixels, dtype= np.float32)
         return ['hires_density']
 
-class PerturbedField:
+
+class PerturbedField(InitialConditions):
     """
     A class containing all perturbed field boxes
     """
-    def __init__(self, n):
-        self.density = np.zeros(n)
-        self.velocity = np.zeros(n)
+    _id = "InitialConditions" # Makes it look at the InitialConditions files for writing.
+
+    def __init__(self, user_params, cosmo_params, redshift):
+        super().__init__(user_params, cosmo_params, redshift=float(redshift))
+
+        # Extend its group name to include the redshift, so that
+        self._group += "_z%.4f"%self.redshift
+
+    def _init_boxes(self):
+        self.density = np.zeros(self.user_params.HII_tot_num_pixels, dtype=np.float32)
+        self.velocity = np.zeros(self.user_params.HII_tot_num_pixels, dtype=np.float32)
+        return ['density', 'velocity']
 
 
 # ======================================================================================================================
@@ -190,35 +200,112 @@ def initial_conditions(user_params=UserParams(), cosmo_params=CosmoParams(), reg
     return boxes
 
 
-# def perturb_field(redshift, init_boxes,  write=True, regenerate=False, read=False):
-#     """
-#     Compute a perturbed field at a given redshift.
-#
-#     Parameters
-#     ----------
-#     redshift
-#     init_boxes
-#     write
-#     regenerate
-#     read
-#     dir
-#
-#     Returns
-#     -------
-#
-#     """
-#     # First initialize perturbed boxes.
-#     fields = PerturbedField(len(init_boxes.lowres_vx))
-#
-#     # Run the C Code
-#     lib.ComputePerturbField(redshift, init_boxes, fields)
-#
-#     # Optionally do stuff with the result (like writing it)
-#     if write:
-#         pass
-#
-#     return fields
-#
+def perturb_field(redshift, init_boxes=None, user_params=None, cosmo_params=None,
+                  regenerate=False, write=True, direc=None,
+                  fname=None, match_seed=False):
+    """
+    Compute a perturbed field at a given redshift.
+
+    Parameters
+    ----------
+    redshift : float
+        The redshift at which to compute the perturbed field.
+
+    init_boxes : :class:`~InitialConditions` instance, optional
+        If given, these initial conditions boxes will be used, otherwise initial conditions will be generated. If given,
+        the user and cosmo params will be set from this object.
+
+    user_params : `~UserParams` instance, optional
+        Defines the overall options and parameters of the run.
+
+    cosmo_params : `~CosmoParams` instance, optional
+        Defines the cosmological parameters used to compute initial conditions.
+
+
+    regenerate : bool, optional
+        Whether to force regeneration of the initial conditions, even if a corresponding box is found.
+
+    write : bool, optional
+        Whether to write results to file.
+
+    direc : str, optional
+        The directory in which to search for the boxes and write them. By default, this is the centrally-managed
+        directory, given by the ``config.yml`` in ``.21CMMC`.
+
+    fname : str, optional
+        The filename to search for/write to.
+
+    match_seed : bool, optional
+        Whether to force the random seed to also match in order to be considered a match.
+
+    Returns
+    -------
+    :class:`~PerturbField`
+        An object containing the density and velocity fields at the specified redshift.
+
+    Examples
+    --------
+    The simplest method is just to give a redshift::
+
+    >>> field = perturb_field(7.0)
+    >>> print(field.density)
+
+    Doing so will internally call the :func:`~initial_conditions` function. If initial conditions have already been
+    calculated, this can be avoided by passing them:
+
+    >>> init_boxes = initial_conditions()
+    >>> field7 = perturb_field(7.0, init_boxes)
+    >>> field8 = perturb_field(8.0, init_boxes)
+
+    The user and cosmo parameter structures are by default inferred from the ``init_boxes``, so that the following is
+    consistent::
+
+    >>> init_boxes = initial_conditions(user_params= UserParams(HII_DIM=1000))
+    >>> field7 = perturb_field(7.0, init_boxes)
+
+    If ``init_boxes`` is not passed, then these parameters can be directly passed::
+
+    >>> field7 = perturb_field(7.0, user_params=UserParams(HII_DIM=1000))
+
+    """
+    # Try setting the user/cosmo params via the init_boxes
+    if init_boxes is not None:
+        user_params = init_boxes.user_params
+        cosmo_params = init_boxes.cosmo_params
+
+    # Set to defaults if init_boxes wasn't provided and neither were they.
+    user_params = user_params or UserParams()
+    cosmo_params = cosmo_params or CosmoParams()
+
+    # Make sure we've got computed init boxes.
+    if init_boxes is None or not init_boxes.filled:
+        init_boxes = initial_conditions(
+            user_params, cosmo_params, regenerate=regenerate, write=write,
+            direc=direc, fname=fname
+        )
+
+    # Initialize perturbed boxes.
+    fields = PerturbedField(user_params, cosmo_params, redshift)
+
+    # Check whether the boxes already exist
+    if not regenerate:
+        try:
+            fields.read(direc, fname, match_seed=match_seed)
+            print("Existing perturb_field boxes found and read in.")
+            return fields
+        except IOError:
+            pass
+
+    # Run the C Code
+    lib.ComputePerturbField(redshift, init_boxes(), fields())
+    fields.filled = True
+
+    # Optionally do stuff with the result (like writing it)
+    if write:
+        fields.write(direc, fname)
+
+    return fields
+
 #
 # def ionize(redshifts, flag_options, astro_params):
 #     for z in redshifts:
