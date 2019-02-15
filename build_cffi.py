6cca49e084cf820c433d0e87d75378ec2e77a01e
Steven Murray <steven.murray@curtin.edu.au>
2019-02-15 09:16:47 -0700

    more py35 f-string shenanigans

--- build_cffi.py
+++ build_cffi.py
@@ -42,11 +42,11 @@ for k,v in os.environ.items():
 # This is the overall C code.
 ffi.set_source(
     "py21cmmc._21cmfast._21cmfast",  # Name/Location of shared library module
-    f'''
+    '''
     #define LOG_LEVEL {log_level}
     
     #include "GenerateICs.c"
-    ''',
+    '''.format(log_level=log_level),
     include_dirs = include_dirs,
     library_dirs=library_dirs,
     libraries=['m','gsl','gslcblas','fftw3f_omp', 'fftw3f'],
