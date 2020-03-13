2a9a4a0786c0fb715b4f9cb4f95b4743adcfdc82
Yuxiang <Yuxiang.L.Qin@gmail.com>
2020-03-14 03:46:36 +1100

    initialise previous Fcolls

--- src/py21cmfast/src/IonisationBox.c
+++ src/py21cmfast/src/IonisationBox.c
@@ -273,12 +273,82 @@ LOG_SUPER_DEBUG("density field calculated");
                 previous_ionize_box->dNrec_box   = (float *) malloc(sizeof(float)*HII_TOT_NUM_PIXELS);
                 previous_ionize_box->mean_f_coll = 0.0;
                 previous_ionize_box->mean_f_coll_MINI = 0.0;
+
+				// really painful to get the length...
+	        	R=fmax(global_params.R_BUBBLE_MIN, (cell_length_factor*user_params->BOX_LEN/(float)user_params->HII_DIM));
+    	    	while ((R - fmin(astro_params->R_BUBBLE_MAX, L_FACTOR*user_params->BOX_LEN)) <= FRACT_FLOAT_ERR ){
+                    if(R >= fmin(astro_params->R_BUBBLE_MAX, L_FACTOR*user_params->BOX_LEN)) {
+                        stored_R = R/(global_params.DELTA_R_HII_FACTOR);
+                    }
+        		    R*= global_params.DELTA_R_HII_FACTOR;
+				}
+        		R=fmin(astro_params->R_BUBBLE_MAX, L_FACTOR*user_params->BOX_LEN);
+        		counter = 0;
+        		LAST_FILTER_STEP = 0;
+        		first_step_R = 1;
+
+		        while (!LAST_FILTER_STEP && (M_MIN < RtoM(R)) ){
+            		if ( ((R/(global_params.DELTA_R_HII_FACTOR) - cell_length_factor*(user_params->BOX_LEN)/(float)(user_params->HII_DIM)) <= FRACT_FLOAT_ERR) || ((R/(global_params.DELTA_R_HII_FACTOR) - global_params.R_BUBBLE_MIN) <= FRACT_FLOAT_ERR) ) {
+                		LAST_FILTER_STEP = 1;
+		                R = fmax(cell_length_factor*user_params->BOX_LEN/(double)(user_params->HII_DIM), global_params.R_BUBBLE_MIN);
+        		    }
+		            if(first_step_R) {
+        		        R = stored_R;
+                		first_step_R = 0;
+		            }
+        		    else {
+		                R /= (global_params.DELTA_R_HII_FACTOR);
+        		    }
+                	counter += 1;
+        		}
+
+				previous_ionize_box->Fcoll       = (float *) malloc(sizeof(float)*HII_TOT_NUM_PIXELS*counter);
+				previous_ionize_box->Fcoll_MINI  = (float *) malloc(sizeof(float)*HII_TOT_NUM_PIXELS*counter);
+
+				// again...
+	        	R=fmax(global_params.R_BUBBLE_MIN, (cell_length_factor*user_params->BOX_LEN/(float)user_params->HII_DIM));
+    	    	while ((R - fmin(astro_params->R_BUBBLE_MAX, L_FACTOR*user_params->BOX_LEN)) <= FRACT_FLOAT_ERR ){
+                    if(R >= fmin(astro_params->R_BUBBLE_MAX, L_FACTOR*user_params->BOX_LEN)) {
+                        stored_R = R/(global_params.DELTA_R_HII_FACTOR);
+                    }
+        		    R*= global_params.DELTA_R_HII_FACTOR;
+				}
+        		R=fmin(astro_params->R_BUBBLE_MAX, L_FACTOR*user_params->BOX_LEN);
+        		counter = 0;
+        		LAST_FILTER_STEP = 0;
+        		first_step_R = 1;
+
+		        while (!LAST_FILTER_STEP && (M_MIN < RtoM(R)) ){
+            		if ( ((R/(global_params.DELTA_R_HII_FACTOR) - cell_length_factor*(user_params->BOX_LEN)/(float)(user_params->HII_DIM)) <= FRACT_FLOAT_ERR) || ((R/(global_params.DELTA_R_HII_FACTOR) - global_params.R_BUBBLE_MIN) <= FRACT_FLOAT_ERR) ) {
+                		LAST_FILTER_STEP = 1;
+		                R = fmax(cell_length_factor*user_params->BOX_LEN/(double)(user_params->HII_DIM), global_params.R_BUBBLE_MIN);
+        		    }
+		            if(first_step_R) {
+        		        R = stored_R;
+                		first_step_R = 0;
+		            }
+        		    else {
+		                R /= (global_params.DELTA_R_HII_FACTOR);
+        		    }
+					for (i=0; i<user_params->HII_DIM; i++){
+						for (j=0; j<user_params->HII_DIM; j++){
+							for (k=0; k<user_params->HII_DIM; k++){
+								previous_ionize_box->Fcoll[counter * HII_TOT_NUM_PIXELS + HII_R_INDEX(i,j,k)] = 0.0;
+								previous_ionize_box->Fcoll_MINI[counter * HII_TOT_NUM_PIXELS + HII_R_INDEX(i,j,k)] = 0.0;
+							}
+						}
+					}
+                	counter += 1;
+        		}
+
+
                 for (i=0; i<user_params->HII_DIM; i++){
                     for (j=0; j<user_params->HII_DIM; j++){
                         for (k=0; k<user_params->HII_DIM; k++){
                             *((float *)prev_deltax_unfiltered + HII_R_FFT_INDEX(i,j,k)) = -1.5;
-                            previous_ionize_box->Gamma12_box[HII_R_INDEX(x, y, z)] = 0.0;
-                            previous_ionize_box->z_re_box[HII_R_INDEX(x, y, z)] = -1.0;
+                            previous_ionize_box->Gamma12_box[HII_R_INDEX(i, j, k)] = 0.0;
+                            previous_ionize_box->z_re_box[HII_R_INDEX(i, j, k)] = -1.0;
+                            previous_ionize_box->dNrec_box[HII_R_INDEX(i, j, k)] = 0.0;
                         }
                     }
                 }
@@ -420,7 +490,6 @@ LOG_SUPER_DEBUG("sigma table has been initialised");
     else {
         box->mean_f_coll = FgtrM_General(redshift, M_MIN);
     }
-LOG_SUPER_DEBUG("I'm here");
 
     if(isfinite(box->mean_f_coll)==0) {
         LOG_ERROR("Mean collapse fraction is either finite or NaN!");
