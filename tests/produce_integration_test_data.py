f1bbef8aaf264ce40c9dd49d00c0f798b3559ba1
BradGreig <b.greig@student.unimelb.edu.au>
2020-03-27 13:41:59 +1100

    Put back all tests

--- tests/produce_integration_test_data.py
+++ tests/produce_integration_test_data.py
@@ -26,7 +26,6 @@ DATA_PATH = os.path.join(os.path.dirname(__file__), "test_data")
 DEFAULT_USER_PARAMS = {"HII_DIM": 50, "DIM": 150, "BOX_LEN": 100}
 DEFAULT_ZPRIME_STEP_FACTOR = 1.04
 
-"""
 OPTIONS = (
     [12, {}],
     [11, {"zprime_step_factor": 1.02}],
@@ -62,20 +61,6 @@ OPTIONS = (
             "PHOTON_CONS": True,
         },
     ],
-    [
-        8,
-        {
-            "N_THREADS": 2,
-            "USE_FFTW_WISDOM": True,
-            "USE_MASS_DEPENDENT_ZETA": True,
-            "INHOMO_RECO": True,
-            "USE_TS_FLUCT": True,
-            "PHOTON_CONS": True,
-        },
-    ],
-)
-"""
-OPTIONS = (
     [
         8,
         {
