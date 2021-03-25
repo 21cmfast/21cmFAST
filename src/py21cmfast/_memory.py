43a6e8999c92bf39d765442e71286a688518110a
BradGreig <b.greig@student.unimelb.edu.au>
2021-03-25 16:06:40 +1100

    Added memory function for the spin temperature calculation

--- src/py21cmfast/_memory.py
+++ src/py21cmfast/_memory.py
@@ -61,6 +61,12 @@ def estimate_memory_lightcone(
         flag_options=flag_options,
     )
 
+    memory_st = mem_spin_temperature(
+        user_params=user_params,
+        astro_params=astro_params,
+        flag_options=flag_options,
+    )
+
     return {
         "ics_python": memory_ics["python"],
         "ics_c": memory_ics["c"],
@@ -68,6 +74,8 @@ def estimate_memory_lightcone(
         "pf_c": memory_pf["c"],
         "ib_python": memory_ib["python"],
         "ib_c": memory_ib["c"],
+        "st_python": memory_st["python"],
+        "st_c": memory_st["c"],
     }
 
 
@@ -138,6 +146,7 @@ def mem_perturb_field(
     # LOWRES_density_perturb, LOWRES_density_perturb_saved
     num_c_boxes_HII_DIM = 2
 
+    # These are all fftwf complex arrays (thus 2 * size)
     size_c = (
         (2.0 * (np.float32(1.0).nbytes)) * num_c_boxes_HII_DIM * hii_kspace_num_pixels
     )
@@ -147,6 +156,7 @@ def mem_perturb_field(
         # HIRES_density_perturb, HIRES_density_perturb_saved
         num_c_boxes_DIM += 2
 
+        # These are all fftwf complex arrays (thus 2 * size)
         size_c += (2.0 * (np.float32(1.0).nbytes)) * num_c_boxes_DIM * kspace_num_pixels
 
         # For resampled_box (double)
@@ -235,6 +245,99 @@ def mem_ionize_box(
         # M_coll_unfiltered, M_coll_filtered
         num_c_boxes += 2.0
 
+    # These are all fftwf complex arrays (thus 2 * size)
     size_c = (2.0 * (np.float32(1.0).nbytes)) * num_c_boxes * hii_kspace_num_pixels
+
+    return {"python": size_py, "c": size_c}
+
+
+def mem_spin_temperature(
+    *,
+    user_params=None,
+    astro_params=None,
+    flag_options=None,
+):
+    """A function to estimate total memory usage of a spin_temperature call."""
+    """Memory usage of Python IonizedBox class."""
+
+    """All declared HII_DIM boxes"""
+    # Ts_bx, x_e_box, Tk_box, J_21_LW_box
+    num_py_boxes = 4.0
+
+    size_py = num_py_boxes * (user_params.HII_DIM) ** 3
+
+    # These are all float arrays
+    size_py = (np.float32(1.0).nbytes) * size_py
+
+    """Memory usage within SpinTemperatureBox.c"""
+    hii_kspace_num_pixels = (float(user_params.HII_DIM) / 2.0 + 1.0) * (
+        user_params.HII_DIM
+    ) ** 2
+
+    # box, unfiltered_box
+    num_c_boxes = 2.0
+    num_c_boxes_alt = 0.0
+
+    mem_c_interp = 0.0
+
+    if flag_options.USE_MINI_HALOS:
+        # log10_Mcrit_LW_unfiltered, log10_Mcrit_LW_filtered
+        num_c_boxes += 2.0
+
+        num_c_boxes_alt += global_params.NUM_FILTER_STEPS_FOR_Ts
+
+    # There are a bunch of 1 and 2D interpolation tables, but ignore those as they are small relative to 3D grids
+    # I will add any that could end up considerably large.
+
+    if flag_options.USE_MASS_DEPENDENT_ZETA:
+        # delNL0
+        num_c_boxes_alt += global_params.NUM_FILTER_STEPS_FOR_Ts
+
+        # del_fcoll_Rct, m_xHII_low_box, inverse_val_box
+        num_c_boxes_alt += (
+            3.0  # m_xHII_low_box is an int, but I'll count it as 4 bytes just in case
+        )
+
+        # dxheat_dt_box, dxion_source_dt_box, dxlya_dt_box, dstarlya_dt_box
+        num_c_boxes_alt += 2.0 * 4.0  # factor of 2. as these are doubles
+
+        if flag_options.USE_MINI_HALOS:
+            # del_fcoll_Rct_MINI
+            num_c_boxes_alt += 1.0
+
+            # dstarlyLW_dt_box, dxheat_dt_box_MINI, dxion_source_dt_box_MINI
+            # dxlya_dt_box_MINI, dstarlya_dt_box_MINI, dstarlyLW_dt_box_MINI
+            num_c_boxes_alt += (
+                2.0 * 6.0
+            )  # factor of 2. taking into account that these are doubles
+
+            if user_params.USE_INTERPOLATION_TABLES:
+                # log10_SFRD_z_low_table_MINI, SFRD_z_high_table_MINI (factor of 4 as these are float tables)
+                mem_c_interp += (
+                    4.0 * global_params.NUM_FILTER_STEPS_FOR_Ts * 250.0 * 50.0
+                )  # NSFR_low = 250, NMTURN = 50
+
+                mem_c_interp += (
+                    4.0 * global_params.NUM_FILTER_STEPS_FOR_Ts * 200.0 * 50.0
+                )  # NSFR_high = 200, NMTURN = 50
+    else:
+        # delNL0_rev
+        num_c_boxes_alt += global_params.NUM_FILTER_STEPS_FOR_Ts
+
+        if user_params.USE_INTERPOLATION_TABLES:
+            # fcoll_R_grid, dfcoll_dz_grid (factor of 8. as these are double)
+            mem_c_interp += (
+                2.0 * 8.0 * (global_params.NUM_FILTER_STEPS_FOR_Ts * 400 * 400)
+            )  # zpp_interp_points_SFR = 400, dens_Ninterp = 400
+
+            # dens_grid_int_vals
+            num_c_boxes_alt += 0.5  # 0.5 as it is a short
+
+    # These are all fftwf complex arrays (thus 2 * size)
+    size_c = (2.0 * (np.float32(1.0).nbytes)) * num_c_boxes * hii_kspace_num_pixels
+
+    size_c += (np.float32(1.0).nbytes) * num_c_boxes_alt * (user_params.HII_DIM ** 3.0)
+
+    size_c += mem_c_interp
 
     return {"python": size_py, "c": size_c}
