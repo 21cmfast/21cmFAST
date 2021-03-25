6860415bf3d7da1175719bdfe06cd942e4345369
BradGreig <b.greig@student.unimelb.edu.au>
2021-03-25 13:07:00 +1100

    Add function for perturb_field

--- src/py21cmfast/_memory.py
+++ src/py21cmfast/_memory.py
@@ -53,14 +53,22 @@ def estimate_memory_lightcone(
     """Compute an estimate of the requisite memory needed by the user for a run_lightcone call."""
     memory_ics = mem_initial_conditions(user_params=user_params)
 
-    return memory_ics
+    memory_pf = mem_perturb_field(user_params=user_params)
+
+    return {
+        "ics_python": memory_ics["python"],
+        "ics_c": memory_ics["c"],
+        "pf_python": memory_pf["python"],
+        "pf_c": memory_pf["c"],
+    }
 
 
 def mem_initial_conditions(
     *,
     user_params=None,
 ):
-    """Memory usage of Python initial conditions class."""
+    """A function to estimate total memory usage of an initial_conditions call."""
+    """Memory usage of Python InitialConditions class."""
     """All declared HII_DIM boxes"""
     # lowres_density, lowres_vx, lowres_vy, lowres_vz, lowres_vcb
     # lowres_vx_2LPT, lowres_vy_2LPT, lowres_vz_2LPT
@@ -77,19 +85,66 @@ def mem_initial_conditions(
     # These are all float arrays
     size_py = (np.float32(1.0).nbytes) * size_py
 
-    """Memory usage of C initial conditions"""
+    """Memory usage within GenerateICs"""
     kspace_num_pixels = (float(user_params.DIM) / 2.0 + 1.0) * (user_params.DIM) ** 2
 
     """All declared DIM boxes"""
     # HIRES_box, HIRES_box_saved
     num_c_boxes = 2
 
-    """All declared fftwf boxes (DIM)"""
+    """All declared 2LPT boxes (DIM)"""
     # phi_1 (6 components)
     if global_params.SECOND_ORDER_LPT_CORRECTIONS:
         num_c_boxes += 6
 
     # These are all fftwf complex arrays (thus 2 * size)
     size_c = (2.0 * (np.float32(1.0).nbytes)) * num_c_boxes * kspace_num_pixels
+
+    return {"python": size_py, "c": size_c}
+
+
+def mem_perturb_field(
+    *,
+    user_params=None,
+):
+    """A function to estimate total memory usage of a perturb_field call."""
+    """Memory usage of Python PerturbedField class."""
+    """All declared HII_DIM boxes"""
+    # density, velocity
+    num_py_boxes_HII_DIM = 2.0
+
+    size_py = num_py_boxes_HII_DIM * (user_params.HII_DIM) ** 3
+
+    # These are all float arrays
+    size_py = (np.float32(1.0).nbytes) * size_py
+
+    """Memory usage within PerturbField.c"""
+    kspace_num_pixels = (float(user_params.DIM) / 2.0 + 1.0) * (user_params.DIM) ** 2
+    hii_kspace_num_pixels = (float(user_params.HII_DIM) / 2.0 + 1.0) * (
+        user_params.HII_DIM
+    ) ** 2
+
+    tot_num_pixels = float(user_params.DIM) ** 3
+    hii_tot_num_pixels = float(user_params.HII_DIM) ** 3
+
+    # LOWRES_density_perturb, LOWRES_density_perturb_saved
+    num_c_boxes_HII_DIM = 2
+
+    size_c = (
+        (2.0 * (np.float32(1.0).nbytes)) * num_c_boxes_HII_DIM * hii_kspace_num_pixels
+    )
+
+    num_c_boxes_DIM = 0
+    if user_params.PERTURB_ON_HIGH_RES:
+        # HIRES_density_perturb, HIRES_density_perturb_saved
+        num_c_boxes_DIM += 2
+
+        size_c += (2.0 * (np.float32(1.0).nbytes)) * num_c_boxes_DIM * kspace_num_pixels
+
+        # For resampled_box (double)
+        size_c += (np.float64(1.0).nbytes) * tot_num_pixels
+    else:
+        # For resampled_box (double)
+        size_c += (np.float64(1.0).nbytes) * hii_tot_num_pixels
 
     return {"python": size_py, "c": size_c}
