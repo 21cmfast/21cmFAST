1b9355d3768451e3c20bef352121893592ec6367
BradGreig <b.greig@student.unimelb.edu.au>
2021-03-26 15:30:42 +1100

    Add in the calculation of the memory for the lightcone function

--- src/py21cmfast/_memory.py
+++ src/py21cmfast/_memory.py
@@ -1,6 +1,7 @@
 """Function to estimate total memory usage."""
 
 import numpy as np
+from copy import deepcopy
 
 from .inputs import AstroParams, CosmoParams, FlagOptions, UserParams, global_params
 from .wrapper import _logscroll_redshifts, _setup_lightcone
@@ -55,27 +56,67 @@ def estimate_memory_lightcone(
     lightcone_quantities=("brightness_temp",),
 ):
     """Compute an estimate of the requisite memory needed by the user for a run_lightcone call."""
+    # First, calculate the memory usage for the initial conditions
     memory_ics = mem_initial_conditions(user_params=user_params)
 
+    memory_data = {"ics_%s" % k: memory_ics["%s" % k] for k in memory_ics.keys()}
+
+    # Maximum memory while running ICs
+    peak_memory = memory_ics["c"] + memory_ics["python"]
+
+    # Now the perturb field
     memory_pf = mem_perturb_field(user_params=user_params)
 
-    memory_ib = mem_ionize_box(
-        user_params=user_params,
-        astro_params=astro_params,
-        flag_options=flag_options,
-    )
+    memory_data.update({"pf_%s" % k: memory_pf["%s" % k] for k in memory_pf.keys()})
 
-    memory_st = mem_spin_temperature(
-        user_params=user_params,
-        astro_params=astro_params,
-        flag_options=flag_options,
-    )
+    # Stored ICs in python + allocated C and Python memory for perturb_field
+    current_memory = memory_ics["python"] + memory_pf["python"] + memory_pf["c"]
 
-    memory_bt = mem_brightness_temperature(user_params=user_params)
+    # Check if running perturb_field requires more memory than generating ICs
+    peak_memory = peak_memory if peak_memory > current_memory else current_memory
 
-    memory_hf = mem_halo_field(user_params=user_params)
+    # If we are using the photon non-conservation correction
+    if flag_options.PHOTON_CONS:
+        # First need to create new structs for photon the photon-conservation
+        astro_params_photoncons = deepcopy(astro_params)
+        astro_params_photoncons._R_BUBBLE_MAX = astro_params.R_BUBBLE_MAX
 
-    memory_phf = mem_perturb_halo(user_params=user_params)
+        flag_options_photoncons = FlagOptions(
+            USE_MASS_DEPENDENT_ZETA=flag_options.USE_MASS_DEPENDENT_ZETA,
+            M_MIN_in_Mass=flag_options.M_MIN_in_Mass,
+            USE_VELS_AUX=user_params.USE_RELATIVE_VELOCITIES,
+        )
+
+        # First perturb_field
+        memory_pf = mem_perturb_field(user_params=user_params)
+
+        # First ionize_box
+        memory_ib = mem_ionize_box(
+            user_params=user_params,
+            astro_params=astro_params_photoncons,
+            flag_options=flag_options_photoncons,
+        )
+
+        # As we iterate through we storing the python memory of two
+        # perturb_field and ionize_boxes plus the C memory of either
+        # of the ionize_box or perturb field as it is being calculated
+        peak_memory_photoncons = memory_ics[
+            "python"
+        ]  # We have the initial conditions in memory
+        peak_memory_photoncons += 2 * (
+            memory_pf["python"] + memory_ib["python"]
+        )  # The python memory
+        peak_memory_photoncons += (
+            memory_pf["c"] if memory_pf["c"] > memory_ib["c"] else memory_ib["c"]
+        )  # Maximum C memory, as it is freed after usage
+
+        # Check if the memory required to do the photon non-conservation correction exceeds
+        # current peak memory usage
+        peak_memory = (
+            peak_memory
+            if peak_memory > peak_memory_photoncons
+            else peak_memory_photoncons
+        )
 
     # Now need to determine the size of the light-cone and how many types are to be stored in memory.
     # Below are taken from the run_lightcone function.
@@ -112,22 +153,110 @@ def estimate_memory_lightcone(
     size_lightcones = n_lightcone * (user_params.HII_DIM) ** 2
     size_lightcones = (np.float32(1.0).nbytes) * num_lightcones * size_lightcones
 
-    return {
-        "ics_python": memory_ics["python"],
-        "ics_c": memory_ics["c"],
-        "pf_python": memory_pf["python"],
-        "pf_c": memory_pf["c"],
-        "ib_python": memory_ib["python"],
-        "ib_c": memory_ib["c"],
-        "st_python": memory_st["python"],
-        "st_c": memory_bt["c"],
-        "bt_python": memory_st["python"],
-        "bt_c": memory_bt["c"],
-        "hf_python": memory_hf["python"],
-        "hf_c": memory_hf["c"],
-        "phf_python": memory_phf["python"],
-        "phf_c": memory_phf["c"],
-    }
+    memory_data.update({"python_lc": size_lightcones})
+
+    # All the data kept in memory at this point in Python
+    current_memory = memory_ics["python"] + memory_pf["python"] + size_lightcones
+
+    # Check if we now exceed the peak memory usage thus far
+    peak_memory = peak_memory if peak_memory > current_memory else current_memory
+
+    # Now start generating the data to populate the light-cones
+
+    # Calculate the memory for a determine_halo_list call
+    memory_hf = mem_halo_field(user_params=user_params)
+
+    memory_data.update({"hf_%s" % k: memory_hf["%s" % k] for k in memory_hf.keys()})
+
+    # Calculate the memory for a perturb_halo_list call
+    memory_phf = mem_perturb_halo(user_params=user_params)
+
+    memory_data.update({"phf_%s" % k: memory_phf["%s" % k] for k in memory_phf.keys()})
+
+    # Calculate the memory for an ionize_box call
+    memory_ib = mem_ionize_box(
+        user_params=user_params,
+        astro_params=astro_params,
+        flag_options=flag_options,
+    )
+
+    memory_data.update({"ib_%s" % k: memory_ib["%s" % k] for k in memory_ib.keys()})
+
+    # Calculate the memory for a spin_temperature call
+    memory_st = mem_spin_temperature(
+        user_params=user_params,
+        astro_params=astro_params,
+        flag_options=flag_options,
+    )
+
+    memory_data.update({"st_%s" % k: memory_st["%s" % k] for k in memory_st.keys()})
+
+    # Calculate the memory for a brightness_temperature call
+    memory_bt = mem_brightness_temperature(user_params=user_params)
+
+    memory_data.update({"bt_%s" % k: memory_bt["%s" % k] for k in memory_bt.keys()})
+
+    # Now, when calculating the light-cone we always need two concurrent boxes (current and previous redshift)
+
+    # This is the all the data currently in memory at this point
+    stored_memory = current_memory  # Corresponds to ICs, one perturb_field and the allocated light-cones
+
+    # Add second perturb_field
+    stored_memory += memory_pf["python"]
+
+    # Add the memory from perturb_haloes
+    if flag_options.USE_HALO_FIELD:
+        # We'll have two copies of this at any one time, one of the original haloes
+        # and one for the current redshift we are evaluating
+        # Note this is approximate as we can only guess the total memory due to the
+        # fact that the amount of memory required is proportional to the number of haloes
+        # found.
+        stored_memory += 2.0 * memory_phf["c"]
+
+    # Add the two ionized boxes
+    stored_memory += 2.0 * memory_ib["python"]
+
+    # Add (if necessary) the two spin temperature boxes
+    if flag_options.USE_TS_FLUCT:
+        stored_memory += 2.0 * memory_st["python"]
+
+        # We also have initialied C memory that is retained until the end of the calculation
+        stored_memory += memory_st["c_init"]
+
+    # Add the two brightness_temperature boxes
+    stored_memory += 2.0 * memory_bt["python"]
+
+    # Now we have an estimate for the data retained in memory, now we just need to check
+    # the peak memory usage (which includes the additional C memory that is allocated
+    # and then freed on a single redshift call)
+    # First check perturb field
+    current_memory = stored_memory + memory_pf["c"]  # Add the temporary C memory
+
+    # Check if our peak memory usage has been exceeded
+    peak_memory = peak_memory if peak_memory > current_memory else current_memory
+
+    # Check spin temperature
+    if flag_options.USE_TS_FLUCT:
+        current_memory = stored_memory + memory_st["c_per_z"]
+
+        # Check if our peak memory usage has been exceeded
+        peak_memory = peak_memory if peak_memory > current_memory else current_memory
+
+    # Check ionized box
+    current_memory = stored_memory + memory_ib["c"]
+
+    # Check if our peak memory usage has been exceeded
+    peak_memory = peak_memory if peak_memory > current_memory else current_memory
+
+    # Check brightness temperature
+    current_memory = stored_memory + memory_bt["c"]
+
+    # Check if our peak memory usage has been exceeded
+    peak_memory = peak_memory if peak_memory > current_memory else current_memory
+
+    memory_data.update({"peak_memory": peak_memory})
+
+    return memory_data
 
 
 def mem_initial_conditions(
@@ -328,6 +457,7 @@ def mem_spin_temperature(
     # box, unfiltered_box
     num_c_boxes = 2.0
     num_c_boxes_alt = 0.0
+    num_c_boxes_initialised = 0.0
 
     mem_c_interp = 0.0
 
@@ -335,6 +465,7 @@ def mem_spin_temperature(
         # log10_Mcrit_LW_unfiltered, log10_Mcrit_LW_filtered
         num_c_boxes += 2.0
 
+        # log10_Mcrit_LW
         num_c_boxes_alt += global_params.NUM_FILTER_STEPS_FOR_Ts
 
     # There are a bunch of 1 and 2D interpolation tables, but ignore those as they are small relative to 3D grids
@@ -342,23 +473,23 @@ def mem_spin_temperature(
 
     if flag_options.USE_MASS_DEPENDENT_ZETA:
         # delNL0
-        num_c_boxes_alt += global_params.NUM_FILTER_STEPS_FOR_Ts
+        num_c_boxes_initialised += global_params.NUM_FILTER_STEPS_FOR_Ts
 
         # del_fcoll_Rct, m_xHII_low_box, inverse_val_box
-        num_c_boxes_alt += (
+        num_c_boxes_initialised += (
             3.0  # m_xHII_low_box is an int, but I'll count it as 4 bytes just in case
         )
 
         # dxheat_dt_box, dxion_source_dt_box, dxlya_dt_box, dstarlya_dt_box
-        num_c_boxes_alt += 2.0 * 4.0  # factor of 2. as these are doubles
+        num_c_boxes_initialised += 2.0 * 4.0  # factor of 2. as these are doubles
 
         if flag_options.USE_MINI_HALOS:
             # del_fcoll_Rct_MINI
-            num_c_boxes_alt += 1.0
+            num_c_boxes_initialised += 1.0
 
             # dstarlyLW_dt_box, dxheat_dt_box_MINI, dxion_source_dt_box_MINI
             # dxlya_dt_box_MINI, dstarlya_dt_box_MINI, dstarlyLW_dt_box_MINI
-            num_c_boxes_alt += (
+            num_c_boxes_initialised += (
                 2.0 * 6.0
             )  # factor of 2. taking into account that these are doubles
 
@@ -373,7 +504,7 @@ def mem_spin_temperature(
                 )  # NSFR_high = 200, NMTURN = 50
     else:
         # delNL0_rev
-        num_c_boxes_alt += global_params.NUM_FILTER_STEPS_FOR_Ts
+        num_c_boxes_initialised += global_params.NUM_FILTER_STEPS_FOR_Ts
 
         if user_params.USE_INTERPOLATION_TABLES:
             # fcoll_R_grid, dfcoll_dz_grid (factor of 8. as these are double)
@@ -382,16 +513,22 @@ def mem_spin_temperature(
             )  # zpp_interp_points_SFR = 400, dens_Ninterp = 400
 
             # dens_grid_int_vals
-            num_c_boxes_alt += 0.5  # 0.5 as it is a short
+            num_c_boxes_initialised += 0.5  # 0.5 as it is a short
 
     # These are all fftwf complex arrays (thus 2 * size)
     size_c = (2.0 * (np.float32(1.0).nbytes)) * num_c_boxes * hii_kspace_num_pixels
 
     size_c += (np.float32(1.0).nbytes) * num_c_boxes_alt * (user_params.HII_DIM ** 3.0)
 
-    size_c += mem_c_interp
+    size_c_init = (
+        (np.float32(1.0).nbytes)
+        * num_c_boxes_initialised
+        * (user_params.HII_DIM ** 3.0)
+    )
 
-    return {"python": size_py, "c": size_c}
+    size_c_init += mem_c_interp
+
+    return {"python": size_py, "c_init": size_c_init, "c_per_z": size_c}
 
 
 def mem_brightness_temperature(
