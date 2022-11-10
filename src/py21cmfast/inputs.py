266d2c9c4a067121a10ffa1d51baf29d32612010
Junsong Cang <junsong.cang@sns.it>
2022-11-10 11:29:48 +0800

    Removed test

--- src/py21cmfast/inputs.py
+++ src/py21cmfast/inputs.py
@@ -1017,5 +1017,3 @@ def validate_all_inputs(
         logger.warning(
             "USE_MINI_HALOS needs USE_RELATIVE_VELOCITIES to get the right evolution!"
         )
-    if flag_options is not None and (flag_options.USE_RADIO_ACG is True):
-        raise ValueError("Testing error module, ACG not used")
