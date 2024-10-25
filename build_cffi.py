b939e36678d839da01b281e2e35ae785750b07e2
alserene <aserene@swin.edu.au>
2024-10-25 12:28:41 +1100

    Add PerturbField.o to build file.

--- build_cffi.py
+++ build_cffi.py
@@ -29,7 +29,11 @@ c_files = [
 
 # compiled cuda code
 # extra_objects = [os.path.join(CLOC, "hello_world.o")]
-extra_objects = [os.path.join(CLOC, "hello_world.o"), os.path.join(CLOC, "filtering.o")]
+extra_objects = [
+    os.path.join(CLOC, "hello_world.o"),
+    os.path.join(CLOC, "filtering.o"),
+    os.path.join(CLOC, "PerturbField.o"),
+    ]
 extra_link_args = ["-lcudart"]
 
 # Set the C-code logging level.
