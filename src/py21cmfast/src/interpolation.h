ea41f8ff7bef4bdfb38113f98b0094f714874fa3
JHu <jitinghu@swin.edu.au>
2024-12-02 08:48:37 +1100

    separate type def from function prototypes (tmp solution)

--- src/py21cmfast/src/interpolation.h
+++ src/py21cmfast/src/interpolation.h
@@ -3,59 +3,28 @@
 
 #include <stdbool.h>
 
-typedef struct RGTable1D{
-    int n_bin;
-    double x_min;
-    double x_width;
+#include "interpolation_types.h"
 
-    double *y_arr;
-    bool allocated;
-}RGTable1D;
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    void allocate_RGTable1D(int n_bin, RGTable1D *ptr);
+    void allocate_RGTable1D_f(int n_bin, RGTable1D_f *ptr);
+    void allocate_RGTable2D(int n_x, int n_y, RGTable2D *ptr);
+    void allocate_RGTable2D_f(int n_x, int n_y, RGTable2D_f *ptr);
 
-typedef struct RGTable2D{
-    int nx_bin, ny_bin;
-    double x_min, y_min;
-    double x_width, y_width;
+    void free_RGTable1D(RGTable1D *ptr);
+    void free_RGTable1D_f(RGTable1D_f *ptr);
+    void free_RGTable2D(RGTable2D *ptr);
+    void free_RGTable2D_f(RGTable2D_f *ptr);
 
-    double **z_arr;
-
-    double saved_ll, saved_ul; //for future acceleration
-    bool allocated;
-}RGTable2D;
-
-typedef struct RGTable1D_f{
-    int n_bin;
-    double x_min;
-    double x_width;
-
-    float *y_arr;
-    bool allocated;
-}RGTable1D_f;
-
-typedef struct RGTable2D_f{
-    int nx_bin, ny_bin;
-    double x_min, y_min;
-    double x_width, y_width;
-
-    float **z_arr;
-
-    double saved_ll, saved_ul; //for future acceleration
-    bool allocated;
-}RGTable2D_f;
-
-void allocate_RGTable1D(int n_bin, RGTable1D * ptr);
-void allocate_RGTable1D_f(int n_bin, RGTable1D_f * ptr);
-void allocate_RGTable2D(int n_x, int n_y, RGTable2D * ptr);
-void allocate_RGTable2D_f(int n_x, int n_y, RGTable2D_f * ptr);
-
-void free_RGTable1D(RGTable1D * ptr);
-void free_RGTable1D_f(RGTable1D_f * ptr);
-void free_RGTable2D(RGTable2D * ptr);
-void free_RGTable2D_f(RGTable2D_f * ptr);
-
-double EvaluateRGTable1D(double x, RGTable1D *table);
-double EvaluateRGTable2D(double x, double y, RGTable2D *table);
-double EvaluateRGTable1D_f(double x, RGTable1D_f *table);
-double EvaluateRGTable2D_f(double x, double y, RGTable2D_f *table);
+    double EvaluateRGTable1D(double x, RGTable1D *table);
+    double EvaluateRGTable2D(double x, double y, RGTable2D *table);
+    double EvaluateRGTable1D_f(double x, RGTable1D_f *table);
+    double EvaluateRGTable2D_f(double x, double y, RGTable2D_f *table);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
