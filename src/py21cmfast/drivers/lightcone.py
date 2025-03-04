3a53a68211900e4f3578d603c0f6e2f247298105
James Davies <james.davies@sns.it>
2025-03-04 16:26:46 +0100

    add turnovers to global properties, set default halomass correction to slightly better value, fix plotting inputs

--- src/py21cmfast/drivers/lightcone.py
+++ src/py21cmfast/drivers/lightcone.py
@@ -459,9 +459,18 @@ def _run_lightcone_from_perturbed_fields(
     ):
         # Save mean/global quantities
         for quantity in global_quantities:
-            lightcone.global_quantities[quantity][iz] = np.mean(
-                getattr(coeval, quantity)
-            )
+            if "log10_mturn_acg" == quantity:
+                lightcone.global_quantities[quantity][
+                    iz
+                ] = coeval.ionized_box.log10_Mturnover_ave
+            elif "log10_mturn_mcg" == quantity:
+                lightcone.global_quantities[quantity][
+                    iz
+                ] = coeval.ionized_box.log10_Mturnover_MINI_ave
+            else:
+                lightcone.global_quantities[quantity][iz] = np.mean(
+                    getattr(coeval, quantity)
+                )
 
         # Update photon conservation data in-place
         lightcone.photon_nonconservation_data |= coeval.photon_nonconservation_data
