4ba69bd035336ac69323f2428b5beb4ddd15ac76
James Davies <james.davies@sns.it>
2025-03-18 14:36:51 +0100

    improve test coverage

--- tests/test_cfuncs.py
+++ tests/test_cfuncs.py
@@ -4,6 +4,7 @@ import numpy as np
 import pytest
 
 import py21cmfast as p21c
+from py21cmfast.wrapper import cfuncs as cf
 
 
 def test_run_lf():
@@ -45,3 +46,81 @@ def test_run_tau():
     )
 
     assert tau
+
+
+def test_bad_integral_inputs(default_input_struct):
+    # make arrays with different shapes
+    redshifts = np.linspace(6, 35, num=20)
+    lnM_base = np.linspace(7, 13, num=30)
+    densities = np.linspace(-1, 3, num=25)
+
+    with pytest.raises(ValueError, match="the shapes of"):
+        cf.integrate_chmf_interval(
+            inputs=default_input_struct,
+            redshift=redshifts[0],
+            lnm_lower=lnM_base,
+            lnm_upper=lnM_base[1:],
+            cond_values=densities,
+        )
+
+    with pytest.raises(ValueError, match="the shapes of"):
+        cf.evaluate_inverse_table(
+            inputs=default_input_struct,
+            redshift=redshifts[0],
+            cond_array=densities,
+            probabilities=np.ones(len(densities) - 1),
+        )
+
+    with pytest.raises(ValueError, match="the shapes of"):
+        cf.evaluate_SFRD_z(
+            inputs=default_input_struct,
+            redshifts=redshifts,
+            log10mturns=lnM_base,
+        )
+
+    with pytest.raises(ValueError, match="the shapes of"):
+        cf.evaluate_Nion_z(
+            inputs=default_input_struct,
+            redshifts=redshifts,
+            log10mturns=lnM_base,
+        )
+
+    with pytest.raises(ValueError, match="the shapes of"):
+        cf.evaluate_SFRD_cond(
+            inputs=default_input_struct,
+            redshift=redshifts[0],
+            densities=densities,
+            log10mturns=lnM_base,
+            radius=10.0,
+        )
+
+    with pytest.raises(ValueError, match="the shapes of"):
+        cf.evaluate_Nion_cond(
+            inputs=default_input_struct,
+            redshift=redshifts[0],
+            densities=densities,
+            l10mturns_acg=lnM_base,
+            l10mturns_mcg=lnM_base,
+            radius=10.0,
+        )
+
+    with pytest.raises(ValueError, match="the shapes of"):
+        cf.evaluate_Xray_cond(
+            inputs=default_input_struct,
+            redshift=redshifts[0],
+            densities=densities,
+            log10mturns=lnM_base,
+            radius=10.0,
+        )
+
+    with pytest.raises(
+        ValueError, match="Halo masses and rng shapes must be identical."
+    ):
+        cf.convert_halo_properties(
+            inputs=default_input_struct,
+            redshift=redshifts[0],
+            halo_masses=np.zeros(10),
+            star_rng=np.zeros(10),
+            sfr_rng=np.zeros(10),
+            xray_rng=np.zeros(11),
+        )
