2b0a4acc1a3ffeec35bf1a0440e6e201d69d510e
James Davies <james.davies@sns.it>
2025-05-05 21:01:02 +0200

    integration tests pass

--- tests/test_integration_features.py
+++ tests/test_integration_features.py
@@ -158,7 +158,7 @@ def test_power_spectra_lightcone(name, module_direc, plt, benchmark):
     # For globals, we should assert that they are close
     for key, value in true_global.items():
         print(f"Testing Global {key}")
-        assert np.allclose(value, getattr(lc, key), atol=0, rtol=1e-3)
+        assert np.allclose(value, lc.global_quantities[key], atol=0, rtol=1e-3)
 
 
 def make_lightcone_comparison_plot(
