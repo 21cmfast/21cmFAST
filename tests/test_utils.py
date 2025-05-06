7f0301eac0d7637590e23c00b1707754ec3bd7b5
James Davies <james.davies@sns.it>
2025-05-06 12:45:53 +0200

    complete coverage

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -24,3 +24,13 @@ def test_ref_printing():
     assert "10.1093/mnras/staa1131" in ref_str
     assert "10.1093/mnras/stac185" in ref_str
     assert "10.48550/arXiv.2504.17254" in ref_str
+
+    inputs = InputParameters.from_template("simple", random_seed=1234)
+    ref_str = show_references(inputs, print_to_stdout=True)
+
+    assert "2011MNRAS.411..955M" in ref_str
+    assert "10.21105/joss.02582" in ref_str
+    assert "10.1093/mnras/stz032" not in ref_str
+    assert "10.1093/mnras/staa1131" not in ref_str
+    assert "10.1093/mnras/stac185" not in ref_str
+    assert "10.48550/arXiv.2504.17254" not in ref_str
