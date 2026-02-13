c227e2ba1fedb1f8e5f0028808fdf587d5ed57a8
Steven Murray <murray.steveng@gmail.com>
2026-02-13 22:32:08 +0200

    fix: mission optional

--- src/py21cmfast/wrapper/outputs.py
+++ src/py21cmfast/wrapper/outputs.py
@@ -1366,11 +1366,11 @@ class IonizedBox(OutputStructZ):
 
     neutral_fraction = _arrayfield()
     ionisation_rate_G12 = _arrayfield()
-    mean_free_path = _arrayfield()  # never used
+    mean_free_path = _arrayfield()
     z_reion = _arrayfield()
     cumulative_recombinations = _arrayfield(optional=True)
-    kinetic_temperature = _arrayfield()  # never used
-    unnormalised_nion = _arrayfield()  # not sure about this one
+    kinetic_temperature = _arrayfield()
+    unnormalised_nion = _arrayfield(optional=True)
     unnormalised_nion_mini = _arrayfield(optional=True)
     log10_Mturnover_ave: float = attrs.field(default=None)
     log10_Mturnover_MINI_ave: float = attrs.field(default=None)
