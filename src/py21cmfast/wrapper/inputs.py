6fbdfa8ee379395bf9871da1320909050b842aca
Steven Murray <murray.steveng@gmail.com>
2026-03-06 15:34:40 +0200

    fix: bug in which cstruct attrs weren't being set for CosmoTables

--- src/py21cmfast/wrapper/inputs.py
+++ src/py21cmfast/wrapper/inputs.py
@@ -376,7 +376,7 @@ class CosmoTables:
             val = getattr(self, k)
             if isinstance(val, Table1D):
                 setattr(self.struct.cstruct, k, val.cstruct)
-            elif isinstance(val, (float | bool)):
+            else:
                 setattr(self.struct.cstruct, k, val)
 
         return self.struct.cstruct
