975cf45914be8f5147b8e63895024e0e7b72f450
orbisai0security <mediratta01.pally@gmail.com>
2026-06-03 08:41:51 +0000

    fix: add buffer-length check in dft.c
    The dft

--- src/py21cmfast/src/dft.c
+++ src/py21cmfast/src/dft.c
@@ -24,7 +24,7 @@ int dft_c2r_cube(bool use_wisdom, int dim, int dim_los, int n_threads, fftwf_com
     Try {
         if (use_wisdom) {
             // Check to see if the wisdom exists
-            sprintf(wisdom_filename, "%s/c2r_DIM%d_DIM%d_NTHREADS%d", config_settings.wisdoms_path,
+            snprintf(wisdom_filename, sizeof(wisdom_filename), "%s/c2r_DIM%d_DIM%d_NTHREADS%d", config_settings.wisdoms_path,
                     dim, dim_los, n_threads);
 
             if (fftwf_import_wisdom_from_filename(wisdom_filename) != 0) {
@@ -52,7 +52,7 @@ int dft_r2c_cube(bool use_wisdom, int dim, int dim_los, int n_threads, fftwf_com
     Try {
         if (use_wisdom) {
             // Check to see if the wisdom exists
-            sprintf(wisdom_filename, "%s/r2c_DIM%d_DIM%d_NTHREADS%d", config_settings.wisdoms_path,
+            snprintf(wisdom_filename, sizeof(wisdom_filename), "%s/r2c_DIM%d_DIM%d_NTHREADS%d", config_settings.wisdoms_path,
                     dim, dim_los, n_threads);
 
             if (fftwf_import_wisdom_from_filename(wisdom_filename) != 0) {
