ca7491a79a75012825af5692648603ccca619364
Jordan Flitter <yflitter@gmail.com>
2026-09-25 15:26:43 +0300

    fixed tests
    Previously, due to misleading instructions, in the configurations no-mdz, mcgs_gamma_approx and ts_nomdz, the HMF parameter was changed to PS in order to avoid the warning 'Your model (either SOURCE_MODEL=='CONST-ION-EFF' or INTEGRATION_METHOD_X=='GAMMA-APPROX')uses the EPS conditional mass function normalised to the unconditional massfunction provided by the user as matter_options.HMF'. These configurations however should be able to work with HMF=ST. Therefore the direct setting of HMF=PS was removed in these configurations. The datafiles were updated to reflect this change.

--- tests/produce_integration_test_data.py
+++ tests/produce_integration_test_data.py
@@ -86,7 +86,7 @@ OPTIONS_TESTRUNS = {
     "simple": [18, {}],
     "no-mdz": [
         18,
-        {"SOURCE_MODEL": "CONST-ION-EFF", "HMF": "PS"},
+        {"SOURCE_MODEL": "CONST-ION-EFF"},
     ],
     "mcgs": [
         18,
@@ -118,7 +118,6 @@ OPTIONS_TESTRUNS = {
             "N_THREADS": 4,
             "INTEGRATION_METHOD_MCGS": "GAMMA-APPROX",
             "INTEGRATION_METHOD_ACGS": "GAMMA-APPROX",
-            "HMF": "PS",
             "V_CB_MODEL": "FLUCTS",
             "POWER_SPECTRUM": "CLASS",
             "K_MAX_FOR_CLASS": 1.0,
@@ -131,7 +130,7 @@ OPTIONS_TESTRUNS = {
     ],
     "ts_nomdz": [
         18,
-        {"USE_TS_FLUCT": True, "SOURCE_MODEL": "CONST-ION-EFF", "HMF": "PS"},
+        {"USE_TS_FLUCT": True, "SOURCE_MODEL": "CONST-ION-EFF"},
     ],
     "homo": [
         18,
